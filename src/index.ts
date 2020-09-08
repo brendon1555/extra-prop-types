0b16dd8d6524d293bd3e25509e742b42a366e4cc
Brendon Lees <brendon1555@gmail.com>
2020-09-08 23:14:35 +1000

    updated dependencies and fixed typing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import createPropType from './createPropType';
 import isColor from './validators/isColor';
+import { Requireable } from 'prop-types';
 
-const colorValidator = createPropType({ validationName: 'color', predicate: propValue => isColor(propValue) });
+const colorValidator = createPropType({ validationName: 'color', predicate: (propValue) => isColor(propValue) });
 
-export const color = colorValidator;
+export const color: Requireable<string> = colorValidator;
